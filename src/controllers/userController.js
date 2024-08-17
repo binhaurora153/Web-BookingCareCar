bd9c9758a8288b57646520b93a4bed451d4bb36e
Thiện Bình <21t1020254@husc.edu.vn>
2024-08-17 10:56:26 +0700

    API

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import userService from "../services/userService";
+
 let handleLogin = async (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
@@ -21,6 +22,45 @@ let handleLogin = async (req, res) => {
   });
 };
 
+// let handleGetAllUsers = async (req, res) => {
+//   let id = req.query.type; //all, id
+
+//   let users = await userService.getAllUsers(id);
+//   console.log(users);
+
+//   return res.status(200).json({
+//     errCode: 0,
+//     errMessage: "OKKK",
+//     users,
+//   });
+// };
+let handleGetAllUsers = async (req, res) => {
+  let id = req.query.id; // Sử dụng query thay vì body
+
+  if (!id) {
+    return res.status(400).json({
+      errCode: 1,
+      message: "Missing input parameter",
+    });
+  }
+
+  try {
+    let users = await userService.getAllUsers(id);
+    return res.status(200).json({
+      errCode: 0,
+      errMessage: "OK",
+      users,
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      errCode: -1,
+      errMessage: "Error from server",
+    });
+  }
+};
+
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -61,6 +61,53 @@ let checkUserEmail = (userEmail) => {
     }
   });
 };
+
+// let getAllUsers = (userId) => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       let users = "";
+//       if (userId === "ALL") {
+//         users = await db.User.findAll({});
+//       }
+
+//       if (userId && userId !== "ALL") {
+//         users = await db.User.findOne({
+//           where: { id: userId },
+//         });
+//       }
+
+//       resolve(users);
+//     } catch (e) {
+//       reject(e);
+//     }
+//   });
+// };
+
+let getAllUsers = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users;
+      if (userId === "ALL") {
+        users = await db.User.findAll({
+          attributes: {
+            exclude: ["password"],
+          },
+        });
+      } else if (userId) {
+        users = await db.User.findOne({
+          where: { id: userId },
+          attributes: {
+            exclude: ["password"],
+          },
+        });
+      }
+      resolve(users);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 module.exports = {
   handleUserLogin: handleUserLogin,
+  getAllUsers: getAllUsers,
 };
