0921c822bf0f240cf49f783dff7ba6eb64720180
Thiện Bình <21t1020254@husc.edu.vn>
2024-09-16 09:14:53 +0700

    fix posidata

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -100,22 +100,28 @@ let getDetailDoctorById = (inputId) => {
             id: inputId,
           },
           attributes: {
-            exclude: ["password", "image"],
+            exclude: ["password"],
           },
           include: [
             {
               model: db.Markdown,
               attributes: ["description", "contentHTML", "contentMarkdown"],
             },
-            // {
-            //   model: db.Markdown,
-            //   as: "positionData",
-            //   attributes: ["valueEn", "valueVi"],
-            // },
+            {
+              model: db.Allcode,
+              as: "positionData",
+              attributes: ["valueEn", "valueVi"],
+            },
           ],
-          raw: true,
+          raw: false,
           nest: true,
         });
+
+        if (data && data.image) {
+          data.image = new Buffer(data.image, "base64").toString("binary");
+        }
+
+        if (!data) data = {};
         resolve({
           errCode: 0,
           data: data,
